54ba435f15f1ea8f7cb90e9af3df667bff6ab9be
sam-andaluri <andaluri@gmail.com>
2021-11-01 14:38:31 +0000

    add pre-reqs.sh

---
+++ anthos-aws/pre-reqs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo apt-get install jq
+
+curl "https://releases.hashicorp.com/terraform/1.0.10/terraform_1.0.10_linux_amd64.zip" -o "terraform.zip"
+unzip terraform.zip
+mv terraform /usr/local/bin
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+gsutil cp gs://gke-multi-cloud-release/aws/aws-1.9.1-gke.0/bin/linux/amd64/anthos-gke .
+chmod 755 anthos-gke
+sudo mv anthos-gke /usr/local/bin
+
